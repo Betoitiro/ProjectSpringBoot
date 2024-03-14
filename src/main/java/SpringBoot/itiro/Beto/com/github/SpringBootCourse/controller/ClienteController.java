879a9d49df55b9d2127d55fa1e4814713b203d1a
Betoitiro <itirotrabalhos@gmail.com>
2024-03-14 09:23:40 -0300

    get by id

--- src/main/java/SpringBoot/itiro/Beto/com/github/SpringBootCourse/controller/ClienteController.java
+++ src/main/java/SpringBoot/itiro/Beto/com/github/SpringBootCourse/controller/ClienteController.java
@@ -2,6 +2,7 @@ package SpringBoot.itiro.Beto.com.github.SpringBootCourse.controller;
 
 import SpringBoot.itiro.Beto.com.github.SpringBootCourse.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,13 @@ public class ClienteController {
     @GetMapping
     public Cliente obterCliente() {
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    //Usando parametros no web Srvice (passando parametros pela url)
+    @GetMapping("/{id}")
+    public  Cliente obterClientePor(@PathVariable  int id)//PathVariable atribui a uma variavel
+    {
+        return new Cliente(id, "Maira ", "987.654.321-00");
     }
 
 }
--- src/main/java/SpringBoot/itiro/Beto/com/github/SpringBootCourse/model/Cliente.java
+++ src/main/java/SpringBoot/itiro/Beto/com/github/SpringBootCourse/model/Cliente.java
@@ -1,18 +1,18 @@
 package SpringBoot.itiro.Beto.com.github.SpringBootCourse.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @Data
-@NoArgsConstructor
 public class Cliente {
     private int id;
     private String name;
     private String cpf;
 
-
+    public Cliente (int id, String name, String cpf){
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+    }
 
 
 }
